1c82d1a24a758ca0c2f94bea0bbce656cda931d8
adibnow-cs <adibnow.cs@gmail.com>
2025-03-01 14:02:26 +0000

    solve nodemon issues

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,6 +12,6 @@ app.get("/api/test", async(req: Request,res: Response)=>{
     res.json({message:"Hello from express endpoint!"});
 });
 
-app.listen(7000)()=> {
-    console.log("Server is running on port 7000");
-}
+app.listen(7000,()=> {
+    console.log("Server is running on localhost: 7000");
+});
