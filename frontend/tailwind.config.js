d3bc08468c80ff356789b96d88abb442d37cc0ce
Adi Bhattacharya <adibnow.cs@gmail.com>
2025-03-24 02:25:52 +0000

    tailwind css setup

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+    // Files to scan for Tailwind class names
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
