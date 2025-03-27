2c544b850d7b865e3f5388dad5b17056c09248a2
Adi Bhattacharya <adibnow.cs@gmail.com>
2025-03-27 12:21:13 +0000

    ,footer and layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,31 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Layout from "./layouts/layout";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Layout></Layout>} />
-        <Route path="/search" element={<>Search Page</>} />
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <p> Home Page</p>
+            </Layout>
+          }
+        />
+        <Route
+          path="/search"
+          element={
+            <Layout>
+              <p> Search Page</p>
+            </Layout>
+          }
+        />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
